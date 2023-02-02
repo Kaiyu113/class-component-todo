91294b320f5f64999242cee433ba9f53651da2ce
Mmandy <Mandy@WingMacBook-Pro.local>
2023-02-01 22:33:22 -0800

    with multiple reducer

--- src/redux/reducers/person.js
+++ src/redux/reducers/person.js
@@ -1,15 +1,14 @@
-import { ADD, SUB } from "./constant";
-const initialState = 99;
+import { ADDP } from "../constant";
+const initialState = [{ name: "TransformStream", age: 1 }];
 
 // Use the initialState as a default value
-export default function calReducer(prestate = initialState, action) {
+export default function personReducer(prestate = initialState, action) {
   // The reducer normally looks at the action type field to decide what happens
   const { type, data } = action;
+
   switch (type) {
-    case ADD:
-      return prestate + data;
-    case SUB:
-      return prestate - data;
+    case ADDP:
+      return [data, ...prestate];
 
     // code block
     // Do something here based on the different types of actions
